8911967323ea0277b22d4be4ae0a1b598b77b197
Zinna <choiyh11097@gmail.com>
2024-03-04 16:41:07 +0900

    [init] Init BaekJoon project

--- README.md
+++ README.md
@@ -8,9 +8,6 @@
 
 > 학습한 알고리즘 문제풀이 지식 정리
 
-### programmers
+### programmers, baekjoon
 
 > 코딩 테스트 문제 풀이 및 테스트 level 별 기록
-
-- `algorithm\src\main\java\com\example\prac` : Java 코딩테스트 문제 풀이
-- `algorithm\src\test\java\com\example\prac`: Junit Test(s)
---
+++ baekjoon/src/main/java/com/example/baekjoon/BaekjoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.baekjoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaekjoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaekjoonApplication.class, args);
+	}
+
+}
