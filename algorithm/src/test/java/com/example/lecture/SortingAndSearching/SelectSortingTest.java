01f37647bd5469ae6c95073f53df3b6a4f4a7e9f
Zinna <choiyh11097@gmail.com>
2024-03-07 13:24:02 +0900

    [test] Update test for select sorting

--- algorithm/src/test/java/com/example/lecture/SortingAndSearching/SelectSortingTest.java
+++ algorithm/src/test/java/com/example/lecture/SortingAndSearching/SelectSortingTest.java
@@ -1,5 +1,22 @@
 package com.example.lecture.SortingAndSearching;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class SelectSortingTest {
+
+    @Test
+    public void testSolution() {
+        SelectSorting selectSorting = new SelectSorting();
+        int[] input1 = { 3, 7, 1, 5, 4, 2, 9, 8, 6 };
+        int[] expected1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        assertArrayEquals(expected1, selectSorting.mySolution(input1.length, input1));
+
+        int[] input2 = { 56, 68, 62, 69, 64, 89, 56, 68, 93, 53 };
+        int[] expected2 = { 53, 56, 56, 62, 64, 68, 68, 69, 89, 93 };
+
+        assertArrayEquals(expected2, selectSorting.mySolution(input2.length, input2));
+    }
 
 }
