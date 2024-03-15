ac96095be6ca4c27a4c653f12a4873c873244f90
Zinna <choiyh11097@gmail.com>
2024-03-15 14:00:21 +0900

    [feat] 알고리즘 강의 8-5. 동전 교환

---
+++ algorithm/src/main/java/com/example/lecture/DFS_BFS/ExchangeCoin.java
@@ -0,0 +1,55 @@
+package com.example.lecture.DFS_BFS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class ExchangeCoin {
+
+    public static int N;
+    public static int M;
+    public static int[] type;
+    public static Queue<int[]> Q = new LinkedList<>();
+
+    public int BFS() {
+
+        Q.offer(new int[] { 0, 0 });
+
+        while (!Q.isEmpty()) {
+            int[] current = Q.poll();
+            int sum = current[0];
+            int L = current[1];
+
+            if (sum == M)
+                return L;
+
+            for (int t : type) {
+                int nextSum = sum + t;
+                if (nextSum <= M) {
+                    Q.offer(new int[] { nextSum, L + 1 });
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        ExchangeCoin T = new ExchangeCoin();
+        Scanner kb = new Scanner(System.in);
+
+        N = kb.nextInt();
+        type = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            type[i] = kb.nextInt();
+        }
+
+        M = kb.nextInt();
+
+        System.out.println(T.BFS());
+
+        kb.close();
+    }
+
+}
