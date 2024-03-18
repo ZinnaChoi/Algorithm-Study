f32332b0fc8b40eebbdb8e36ce1dda00c19aa54f
Zinna <choiyh11097@gmail.com>
2024-03-18 14:51:07 +0900

    [feat] Solving climbing stairs problem using dp array

--- algorithm/src/main/java/com/example/lecture/DP/ClimbingStairs.java
+++ algorithm/src/main/java/com/example/lecture/DP/ClimbingStairs.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class ClimbingStairs {
 
-    public int dp(int N) {
+    static int[] dy;
 
+    public int dp(int N) {
         if (N == 1 || N == 2)
             return N;
         else {
@@ -13,12 +14,26 @@ public class ClimbingStairs {
         }
     }
 
+    public int dp2(int N) {
+        dy[1] = 1;
+        dy[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            dy[i] = dy[i - 2] + dy[i - 1];
+        }
+
+        return dy[N];
+    }
+
     public static void main(String[] args) {
         ClimbingStairs T = new ClimbingStairs();
         Scanner kb = new Scanner(System.in);
 
         int N = kb.nextInt();
+        dy = new int[N + 1];
+
         System.out.println(T.dp(N));
+        // System.out.println(T.dp2(N));
 
         kb.close();
     }
