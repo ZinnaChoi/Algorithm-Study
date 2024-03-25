86f1154b91a0bcc95faca7bc5d3b6ffa6f237680
Zinna <choiyh11097@gmail.com>
2024-03-25 13:56:47 +0900

    [fix] add missing condition

--- algorithm/src/main/java/com/example/lecture/DFS_BFS/TeacherMazeShortestPath.java
+++ algorithm/src/main/java/com/example/lecture/DFS_BFS/TeacherMazeShortestPath.java
@@ -33,7 +33,7 @@ public class TeacherMazeShortestPath {
             for (int i = 0; i < 4; i++) {
                 int nextX = cur.x + dx[i];
                 int nextY = cur.y + dy[i];
-                if (nextX >= 0 && nextY >= 0 && nextX < 7 && nextY < 7) {
+                if (nextX >= 0 && nextY >= 0 && nextX < 7 && nextY < 7 && board[nextX][nextY] == 0) {
                     board[nextX][nextY] = 1;
                     Q.offer(new Point(nextX, nextY));
                     dis[nextX][nextY] = dis[cur.x][cur.y] + 1;
