319dffefb691629334a7b265ed4ee0c86d6187f5
Zinna <choiyh11097@gmail.com>
2024-03-25 13:55:56 +0900

    [feat] Solve maze shortest path problem by saving distance in dis array

---
+++ algorithm/src/main/java/com/example/lecture/DFS_BFS/TeacherMazeShortestPath.java
@@ -0,0 +1,64 @@
+package com.example.lecture.DFS_BFS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class TeacherMazeShortestPath {
+
+    public class Point {
+        public int x, y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+
+    static int[] dx = { -1, 0, 1, 0 };
+    static int[] dy = { 0, 1, 0, -1 };
+    static int[][] board, dis;
+
+    public int BFS() {
+        Queue<Point> Q = new LinkedList<>();
+        Q.offer(new Point(0, 0));
+        board[0][0] = 1;
+
+        while (!Q.isEmpty()) {
+            Point cur = Q.poll();
+            if (cur.x == 6 && cur.y == 6) {
+                return dis[6][6];
+            }
+            for (int i = 0; i < 4; i++) {
+                int nextX = cur.x + dx[i];
+                int nextY = cur.y + dy[i];
+                if (nextX >= 0 && nextY >= 0 && nextX < 7 && nextY < 7) {
+                    board[nextX][nextY] = 1;
+                    Q.offer(new Point(nextX, nextY));
+                    dis[nextX][nextY] = dis[cur.x][cur.y] + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        TeacherMazeShortestPath T = new TeacherMazeShortestPath();
+        Scanner kb = new Scanner(System.in);
+
+        board = new int[7][7];
+        dis = new int[7][7];
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                board[i][j] = kb.nextInt();
+            }
+        }
+
+        System.out.println(T.BFS());
+
+        kb.close();
+    }
+
+}
