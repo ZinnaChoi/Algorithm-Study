9d1e420aa3d6f462a4dd6e9ebdf0965fa367b54c
Zinna <choiyh11097@gmail.com>
2024-04-17 13:26:52 +0900

    [feat] 백준 > 19941번 햄버거 분배(Greedy)

---
+++ baekjoon/src/main/java/com/example/baekjoon/silver/DistributeHamburger.java
@@ -0,0 +1,40 @@
+package com.example.baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.io.InputStreamReader;
+
+public class DistributeHamburger {
+
+    public int getMaxPeopleCnt(int N, int K, char[] loc) {
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            if (loc[i] == 'P') {
+                for (int j = i - K; j <= i + K; j++) {
+                    if (j >= 0 && j < N && loc[j] == 'H') {
+                        loc[j] = 'E';
+                        answer++;
+                        break;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        DistributeHamburger T = new DistributeHamburger();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] line = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int N = line[0];
+        int K = line[1];
+
+        char[] loc = br.readLine().toCharArray();
+
+        System.out.println(T.getMaxPeopleCnt(N, K, loc));
+
+    }
+
+}
