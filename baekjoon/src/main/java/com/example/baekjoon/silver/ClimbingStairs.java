67c04765fb3abe6c309795d86c16f77edc1ae0d4
Zinna <choiyh11097@gmail.com>
2024-04-22 13:28:17 +0900

    [refactor] remove unnecessary logic of climbing stairs problem

--- baekjoon/src/main/java/com/example/baekjoon/silver/ClimbingStairs.java
+++ baekjoon/src/main/java/com/example/baekjoon/silver/ClimbingStairs.java
@@ -7,19 +7,17 @@ import java.io.InputStreamReader;
 public class ClimbingStairs {
 
     public int getMaxScore(int N, int[] stairs) {
+
         if (N == 1)
             return stairs[1];
         if (N == 2)
             return stairs[1] + stairs[2];
-        if (N == 3)
-            return Math.max(stairs[1] + stairs[3], stairs[2] + stairs[3]);
 
         int[] dp = new int[N + 1];
         dp[1] = stairs[1];
         dp[2] = stairs[1] + stairs[2];
-        dp[3] = Math.max(stairs[1] + stairs[3], stairs[2] + stairs[3]);
 
-        for (int i = 4; i <= N; i++) {
+        for (int i = 3; i <= N; i++) {
             dp[i] = Math.max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i - 1] + stairs[i]);
         }
 
