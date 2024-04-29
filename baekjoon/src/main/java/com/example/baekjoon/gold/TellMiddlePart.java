3799d63d409fde22f7e8146afc7b865b94db79f9
Zinna <choiyh11097@gmail.com>
2024-04-29 10:28:15 +0900

    [feat] 백준 > 1655번 가운데를 말해요 (우선순위 큐)

---
+++ baekjoon/src/main/java/com/example/baekjoon/gold/TellMiddlePart.java
@@ -0,0 +1,52 @@
+package com.example.baekjoon.gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class TellMiddlePart {
+
+    public String tellMiddle(int[] arr) {
+
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i];
+
+            if (minHeap.size() == maxHeap.size())
+                maxHeap.offer(num);
+            else
+                minHeap.offer(num);
+
+            if (!minHeap.isEmpty() && !maxHeap.isEmpty())
+                if (minHeap.peek() < maxHeap.peek()) {
+                    int tmp = minHeap.poll();
+                    minHeap.offer(maxHeap.poll());
+                    maxHeap.offer(tmp);
+                }
+
+            sb.append(maxHeap.peek() + "\n");
+        }
+        return sb.toString();
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        TellMiddlePart T = new TellMiddlePart();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(T.tellMiddle(arr));
+    }
+
+}
