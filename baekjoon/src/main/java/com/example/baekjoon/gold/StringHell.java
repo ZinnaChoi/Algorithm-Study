ae5ecfc4219f76555ccf4f371d6b35348f9a05b6
Zinna <choiyh11097@gmail.com>
2024-05-02 10:02:10 +0900

    [feat] 백준 > 20166번 문자열 지옥에 빠진 호석 (DFS)

---
+++ baekjoon/src/main/java/com/example/baekjoon/gold/StringHell.java
@@ -0,0 +1,77 @@
+package com.example.baekjoon.gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class StringHell {
+
+    static int N, M, K;
+    static char[][] map;
+    static int[][][] memo;
+
+    static final int[][] dir = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 }, { 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 } };
+
+    public int DFS(int L, int x, int y, char[] target) {
+        if (L == target.length - 1) {
+            return 1;
+        }
+        if (memo[x][y][L] != -1) {
+            return memo[x][y][L];
+        }
+
+        int cnt = 0;
+        for (int[] d : dir) {
+            int dx = (x + d[0] + N) % N;
+            int dy = (y + d[1] + M) % M;
+            if (map[dx][dy] == target[L + 1]) {
+                cnt += DFS(L + 1, dx, dy, target);
+            }
+        }
+        memo[x][y][L] = cnt;
+        return cnt;
+    }
+
+    public static void main(String[] args) throws IOException {
+        StringHell T = new StringHell();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] nums = br.readLine().split(" ");
+        N = Integer.parseInt(nums[0]);
+        M = Integer.parseInt(nums[1]);
+        K = Integer.parseInt(nums[2]);
+
+        map = new char[N][M];
+        memo = new int[N][M][5];
+
+        for (int n = 0; n < N; n++) {
+            char[] words = br.readLine().toCharArray();
+            for (int m = 0; m < M; m++) {
+                map[n][m] = words[m];
+
+            }
+        }
+
+        for (int i = 0; i < K; i++) {
+
+            for (int n = 0; n < N; n++) {
+                for (int m = 0; m < M; m++) {
+                    for (int l = 0; l < 5; l++) {
+                        memo[n][m][l] = -1;
+                    }
+                }
+            }
+            char[] target = br.readLine().toCharArray();
+            int cnt = 0;
+            for (int n = 0; n < N; n++) {
+                for (int m = 0; m < M; m++) {
+                    if (map[n][m] == target[0]) {
+                        cnt += T.DFS(0, n, m, target);
+                    }
+                }
+            }
+            System.out.println(cnt);
+        }
+    }
+
+}
