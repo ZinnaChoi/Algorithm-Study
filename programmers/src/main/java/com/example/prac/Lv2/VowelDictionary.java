aca53c5c93522cfa96b5430e8a28252a44667a3f
Zinna <choiyh11097@gmail.com>
2024-05-13 11:15:38 +0900

    [feat] 프로그래머스 > 모음 사전 (DFS)

--- programmers/src/main/java/com/example/prac/Lv2/VowelDictionary.java
+++ programmers/src/main/java/com/example/prac/Lv2/VowelDictionary.java
@@ -1,41 +1,29 @@
 package com.example.prac.Lv2;
 
-import java.util.*;
-
 public class VowelDictionary {
+  private static final char[] vowels = { 'A', 'E', 'I', 'O', 'U' };
+  private boolean found = false;
+  private int cnt = 0;
 
-  static char[] currentWord;
-
-  public int solution(String word) {
-    List<String> wordList = new ArrayList<>();
-
-    currentWord = new char[5];
-    DFS(0, wordList);
-
-    Collections.sort(wordList);
-
-    int idx = 1;
-    for (String w : wordList) {
-      if (w.equals(word)) return idx;
-      idx++;
-    }
-    return idx;
-  }
-
-  private void DFS(int L, List<String> lst) {
-    char[] alphabet = { 'A', 'E', 'I', 'O', 'U' };
-
-    if (L > 0) {
-      lst.add(new String(currentWord, 0, L));
-    }
-
-    if (L == 5) {
+  private void DFS(String cur, String target) {
+    if (cur.equals(target)) {
+      found = true;
       return;
     }
+    if (cur.length() >= 5)
+      return;
 
-    for (char letter : alphabet) {
-      currentWord[L] = letter;
-      DFS(L + 1, lst);
+    for (char c : vowels) {
+      if (!found) {
+        cnt++;
+        DFS(cur + c, target);
+      }
     }
   }
+
+  public int solution(String word) {
+    DFS("", word);
+    return cnt;
+  }
+
 }
